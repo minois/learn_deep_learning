2c9716c19f203143508e284957e19f2d5b81626b
Takaaki Minowa <takaaki-minowa@cookpad.jp>
2017-05-02 05:46:56 +0900

    Use num.py for step function

--- step.py
+++ step.py
@@ -2,8 +2,17 @@
 import numpy as np
 
 def step_function(x):
-  if x > 0:
-    return 1
-  else:
-    return 0
+  y = x > 0
+  return y.astype(np.int)
 
+# >>> import numpy as np
+# >>> x = np.array([-1.0, 1.0, 2.0])
+# >>> x
+# array([-1.,  1.,  2.])
+# >>> y = x > 0
+# >>> y
+# array([False,  True,  True], dtype=bool)
+
+# >>> y = y.astype(np.int)
+# >>> y
+# array([0, 1, 1])
